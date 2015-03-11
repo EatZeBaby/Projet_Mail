64e1b859d69f1f5ba9494e38ce6704726bce39a8
stairflyer <elias.arfi@gmail.com>
2015-03-11 15:11:49 +0100

    V1 Personne, Acteur, Real done

--- Projet_Mail/src/cinema/Acteur.java
+++ Projet_Mail/src/cinema/Acteur.java
@@ -1,5 +1,46 @@
 package cinema;
 
+import java.util.ArrayList;
+
 public class Acteur extends Personne{
+	private ArrayList<Film> listeFilms;
+	private ArrayList<Prix> listePrix;
+	private ArrayList<Ceremonie> listeCeremonies;
+	
+
+	public Acteur(String n, String p, String a, String nat) {
+		super(n, p, a, nat);
+		listeFilms = new ArrayList<Film>();
+		listePrix = new ArrayList<Prix> ();
+		listeCeremonies = new ArrayList<Ceremonie>();
+	}
+	
+	public void ajouterFilm(Film f){
+		listeFilms.add(f);
+	}
+	
+	public void ajouterPrix(Prix p){
+		listePrix.add(p);
+	}
+	
+	public void ajouterCeremonie(Ceremonie c){
+		listeCeremonies.add(c);
+	}
+
+	public ArrayList<Film> getListeFilms() {
+		return listeFilms;
+	}
+
+	public void setListeFilms(ArrayList<Film> listeFilms) {
+		this.listeFilms = listeFilms;
+	}
+
+	public ArrayList<Prix> getListePrix() {
+		return listePrix;
+	}
+
+	public void setListePrix(ArrayList<Prix> listePrix) {
+		this.listePrix = listePrix;
+	}
 
 }
---
+++ Projet_Mail/src/cinema/Realisateur.java
@@ -0,0 +1,46 @@
+package cinema;
+
+import java.util.ArrayList;
+
+public class Realisateur extends Personne{
+	private ArrayList<Film> listeFilms;
+	private ArrayList<Prix> listePrix;
+	private ArrayList<Ceremonie> listeCeremonies;
+
+	public Realisateur(String n, String p, String a, String nat) {
+		super(n, p, a, nat);
+		// TODO Auto-generated constructor stub
+		listeFilms = new ArrayList<Film>();
+		listePrix = new ArrayList<Prix> ();
+		listeCeremonies = new ArrayList<Ceremonie>();
+	}
+	
+	public void ajouterFilm(Film f){
+		listeFilms.add(f);
+	}
+	
+	public void ajouterPrix(Prix p){
+		listePrix.add(p);
+	}
+	
+	public void ajouterCeremonie(Ceremonie c){
+		listeCeremonies.add(c);
+	}
+
+	public ArrayList<Film> getListeFilms() {
+		return listeFilms;
+	}
+
+	public void setListeFilms(ArrayList<Film> listeFilms) {
+		this.listeFilms = listeFilms;
+	}
+
+	public ArrayList<Prix> getListePrix() {
+		return listePrix;
+	}
+
+	public void setListePrix(ArrayList<Prix> listePrix) {
+		this.listePrix = listePrix;
+	}
+
+}
