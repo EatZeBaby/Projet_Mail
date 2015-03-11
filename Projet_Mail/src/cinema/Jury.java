f11d539461c72828dd2972d35ec60999982d648a
Axel <axel.richier@gmail.com>
2015-03-11 15:50:47 +0100

    Jury

--- Projet_Mail/src/cinema/Jury.java
+++ Projet_Mail/src/cinema/Jury.java
@@ -4,7 +4,26 @@ import java.util.ArrayList;
 
 public class Jury {
 	
+	
 	private ArrayList <Personne> ListeDesMembresDuJury;
+	private String nomCeremonie;
+	private int annee;
+	
+	public void  getListeDesMembresDuJury() {
+		System.out.println("Les membre du jury de la Ceremonie" + nomCeremonie + );
+		for(Personne membre: this.ListeDesMembresDuJury){
+			System.out.println(membre.getPrenom());
+			} ;
+	}
+	public void setListeDesMembresDuJury(ArrayList <Personne> listeDesMembresDuJury) {
+		ListeDesMembresDuJury = listeDesMembresDuJury;
+	}
+	public int getAnnee() {
+		return annee;
+	}
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
 	
 
 }
