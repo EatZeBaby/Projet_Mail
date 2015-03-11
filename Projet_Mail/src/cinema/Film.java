178fa2c03252d0061ecf73aae0760104d4fb8236
Guimove22 <guillaume.fronczak@gmail.com>
2015-03-11 16:07:24 +0100

    -- Maj Classe Film -- Ajout des méthodes pour ajouter des acteurs / réalisateurs -- Ajout des prix gagnés par le film

--- Projet_Mail/src/cinema/Film.java
+++ Projet_Mail/src/cinema/Film.java
@@ -1,5 +1,6 @@
 package cinema;
 
+import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,15 +10,21 @@ public class Film {
     private String nomFilm;
     private int budgetFilm;
     private List<Acteur> listeActeurs;
+    private List<Realisateur> listeRealisateurs;
+    private List<Prix> listePrix;
+    private Date dateFilm;
 
     /**
      * Constructor for objects of class Film
      */
-    public Film(String NomFilm, int BudgetFilm, List<Acteur> listeActeurs)
+    public Film(String NomFilm, int BudgetFilm, List<Acteur> listeActeurs, List<Realisateur> listeRealisateurs, Date dateFilm)
     {
        this.nomFilm = NomFilm;
        this.budgetFilm = BudgetFilm;
        this.listeActeurs = listeActeurs;
+       this.listeRealisateurs = listeRealisateurs;
+       this.listePrix = new ArrayList<Prix>();
+       this.dateFilm = dateFilm;
     }
 
    
@@ -26,27 +33,91 @@ public class Film {
         System.out.println("Je suis une méthode");
     }
     
-    // Getter
-    public String getNomFilm()
-    {
-        return this.nomFilm;
-    }
+    // Getter et Setter
     
-    public int getBudgetFilm()
-    {
-        return this.budgetFilm;
-    }
+	public String getNomFilm() {
+		return nomFilm;
+	}
+
+
+	public void setNomFilm(String nomFilm) {
+		this.nomFilm = nomFilm;
+	}
+
+
+	public int getBudgetFilm() {
+		return budgetFilm;
+	}
+
+
+	public void setBudgetFilm(int budgetFilm) {
+		this.budgetFilm = budgetFilm;
+	}
+
+
+	public List<Acteur> getListeActeurs() {
+		return listeActeurs;
+	}
+
+
+	public void setListeActeurs(List<Acteur> listeActeurs) {
+		this.listeActeurs = listeActeurs;
+	}
+
+
+	public List<Realisateur> getListeRealisateurs() {
+		return listeRealisateurs;
+	}
+
+
+	public void setListeRealisateurs(List<Realisateur> listeRealisateurs) {
+		this.listeRealisateurs = listeRealisateurs;
+	}
+
+
+	public List<Prix> getListePrix() {
+		return listePrix;
+	}
+
+
+	public void setListePrix(List<Prix> listePrix) {
+		this.listePrix = listePrix;
+	}
+
+
+	public Date getDateFilm() {
+		return dateFilm;
+	}
+
+
+	public void setDateFilm(Date dateFilm) {
+		this.dateFilm = dateFilm;
+	}
     
-   public List getListeActeurs()
-    {
-        return this.listeActeurs;
-    }
+    
+    
+ 
     
     // Méthode retournant les informations générales du film
     public void infoFilms()
     {
         System.out.println("Titre : " + this.nomFilm + " Budget : " + this.budgetFilm);
     }
+    
+    // Ajouter un acteur
+    public void ajouterActeur(Acteur UnActeur)
+    {
+    	this.listeActeurs.add(UnActeur);
+    }
+    
+    // Ajouter un Réalisateur
+    public void ajouterRealisateur(Realisateur UnRealisateur)
+    {
+    	this.listeRealisateurs.add(UnRealisateur);
+    }
+
+
+
     
 
 }
