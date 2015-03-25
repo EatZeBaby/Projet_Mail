da748743ec664404c04e97e37f80cf4c0fc9e8bd
stairflyer <elias.arfi@gmail.com>
2015-03-25 10:25:00 +0100

    ceation prod

---
+++ Projet_Mail/src/cinema/Producteur.java
@@ -0,0 +1,10 @@
+package cinema;
+
+public class Producteur extends Personne{
+
+	public Producteur(String nom, String prenom, int age, String nationalite) {
+		super(nom, prenom, age, nationalite);
+		// TODO Auto-generated constructor stub
+	}
+ // Prout
+}
